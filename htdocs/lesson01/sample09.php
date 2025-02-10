9c54de31d371c69dca6830c5c4853c73e6c5c16b
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-02-10 14:16:43 +0000

    「1年後までの日付を表示」学習途中2

--- htdocs/lesson01/sample09.php
+++ htdocs/lesson01/sample09.php
@@ -1,5 +1,11 @@
 <?php
 date_default_timezone_set('Asia/Tokyo');
-$day = date('n/j(D)');
+
+$time = strtotime('+1 day');
+$day = date('n/j(D)', $time);
+echo $day . '<br>';
+
+$time = strtotime('+1 day');
+$day = date('n/j(D)', $time);
 echo $day . '<br>';
 ?>
