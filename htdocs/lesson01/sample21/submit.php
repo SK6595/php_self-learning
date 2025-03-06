f34d74452cd99086be7a3dfcf76914ace33cbf88
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-03-06 15:14:51 +0000

    「チェックボックスの値の受け取り」学習途中

---
+++ htdocs/lesson01/sample21/submit.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sample21</title>
+</head>
+<body>
+    <h2>ご予約日</h2>
+    <?php $reserves = $_REQUEST['reserve']; ?>
+    <ul>
+        <?php foreach ($reserves as $reserve): ?>
+            <li><?php echo htmlspecialchars($reserve, ENT_QUOTES); ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
