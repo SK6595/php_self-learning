c1b1a0db7cfb8510b36fb14ea129657ffabf791d
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-03-07 22:53:55 +0000

    「チェックボックスの値の受け取り」学習完了

--- htdocs/lesson01/sample21/submit.php
+++ htdocs/lesson01/sample21/submit.php
@@ -7,11 +7,15 @@
 </head>
 <body>
     <h2>ご予約日</h2>
-    <?php $reserves = $_REQUEST['reserve']; ?>
-    <ul>
-        <?php foreach ($reserves as $reserve): ?>
-            <li><?php echo htmlspecialchars($reserve, ENT_QUOTES); ?></li>
-        <?php endforeach; ?>
-    </ul>
+    <?php if (!empty($_REQUEST['reserve'])): ?>
+        <?php $reserves = $_REQUEST['reserve']; ?>
+        <ul>
+            <?php foreach ($reserves as $reserve): ?>
+                <li><?php echo htmlspecialchars($reserve, ENT_QUOTES); ?></li>
+            <?php endforeach; ?>
+        </ul>
+    <?php else: ?>
+        <p>予約日がありません</p>
+    <?php endif; ?> 
 </body>
 </html>
