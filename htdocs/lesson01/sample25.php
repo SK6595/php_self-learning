2b3de64a1451868a122da2b187afaa3b957879e2
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-03-12 23:23:36 +0000

    「剰余算を使用した表の色変え」学習完了

--- htdocs/lesson01/sample25.php
+++ htdocs/lesson01/sample25.php
@@ -8,7 +8,7 @@
 <body>
     <table>
         <?php for ($i = 1; $i <= 10; $i++) : ?>
-            <?php if ($i % 2 === 1): ?>
+            <?php if ($i % 2): ?>
                 <tr style="background-color: #ccc">
             <?php else: ?>
                 <tr>
