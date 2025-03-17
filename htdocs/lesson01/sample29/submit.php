e121f982afeb929e9d5af0e4dac86193a0101e32
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-03-17 23:49:43 +0000

    「ファイルをアップロードしよう」学習途中2

--- htdocs/lesson01/sample29/submit.php
+++ htdocs/lesson01/sample29/submit.php
@@ -1,4 +1,10 @@
 <?php
 $file = $_FILES['picture'];
-var_dump('file');
+$success = move_uploaded_file($file['tmp_name'], $file['name']);
+
+if ($success) {
+    echo '成功しました';
+}  else {
+    echo '失敗しました';
+}
 ?>
