9ccbc0a8af9c929c4ca949dc970d11e8b505e5b7
SK6595 <173334721+SK6595@users.noreply.github.com>
2025-03-18 23:22:24 +0000

    「ファイルをアップロードする」学習完了

--- htdocs/lesson01/sample29/submit.php
+++ htdocs/lesson01/sample29/submit.php
@@ -1,10 +1,17 @@
 <?php
 $file = $_FILES['picture'];
-$success = move_uploaded_file($file['tmp_name'], $file['name']);
 
-if ($success) {
-    echo '成功しました';
-}  else {
-    echo '失敗しました';
+if ($file['type'] === 'image/jpeg' || 
+            $file['type'] === 'image/png') {
+    $path = 'images/' . $file['name'];
+    $success = move_uploaded_file($file['tmp_name'], $path);
+
+    if ($success) {
+        echo '成功しました';
+    }  else {
+        echo '失敗しました';
+    }
+} else {
+    echo 'ファイル形式が不正です';
 }
 ?>
