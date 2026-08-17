f4b4678d5f418c936e9308b200da25211f9102aa
SK6595 <173334721+SK6595@users.noreply.github.com>
2026-08-17 06:24:17 +0000

    「レッスン2：ifによる条件分岐、比較演算子」学習完了

---
+++ htdocs/phpbook/if1.php
@@ -0,0 +1,4 @@
+<?php
+$condition = true; // trueやfalseは文字列ではない（論理値）ため「""」が付かない。
+if ($condition) echo "条件はtrueです。";
+ 
---
+++ htdocs/phpbook/if2.php
@@ -0,0 +1,3 @@
+<?php
+$condition = false;
+if ($conditon) echo "条件はtrueです。";
